d5a1c31d8a9c91290ea86a0ab3bc945e1baa97a7
Mahmoud Ezzat <98676093+mammhoud@users.noreply.github.com>
2024-01-20 00:26:47 +0200

    Update 1.2,3_Sprints.sql

--- 1.2,3_Sprints.sql
+++ 1.2,3_Sprints.sql
@@ -12,30 +12,26 @@ limit 10;
 
 --------------------
 
-SELECT emp.firstname AS Employee_FirstName, emp.lastname AS Employee_LastName, 
-       COALESCE(sum(odd.quantity), 0) AS Sold_Most_Beverages
-FROM w3schools.employees AS emp
-inner JOIN w3schools.orders AS ord ON emp.EmployeeID = ord.EmployeeID
-
-inner JOIN w3schools.order_details AS odd ON odd.OrderID = ord.OrderID
-inner JOIN w3schools.products AS prd ON odd.productid = prd.ProductID
+select emp.firstname as Employee_FirstName, emp.lastname as Employee_LastName, 
+case when sum(odd.quantity) is NULL then 0 
+else sum(odd.Quantity) end as Sold_Most_Beverages
+from w3schools.order_details as odd
+inner join w3schools.orders as ord on odd.OrderID = ord.OrderID
+inner join w3schools.products as prd on odd.productid = prd.ProductID
 inner JOIN w3schools.categories AS cat ON prd.CategoryID = cat.CategoryID and cat.CategoryName = "beverages"
--- WHERE cat.categoryid = "1" --
--- where 
-GROUP BY emp.employeeid
-ORDER BY Sold_Most_Beverages DESC
-LIMIT 10;
+right join w3schools.employees as emp on emp.EmployeeID = ord.EmployeeID
+
+group by emp.employeeid
+order by sold_most_beverages desc
+limit 10;
+
 --------------------
 select emp.firstname as Employee_FirstName, emp.lastname as Employee_LastName, 
--- COALESCE(sum(odd.quantity), 0) as Sold_Most_Beverages
-case when sum(odd.quantity) is NULL then 0 
-    else sum(odd.Quantity) end as Sold_Most_Beverages
---
+COALESCE(sum(odd.quantity), 0) as Sold_Most_Beverages
 from w3schools.employees as emp
 left join w3schools.orders as ord on emp.EmployeeID = ord.EmployeeID
 left join w3schools.order_details as odd on odd.OrderID = ord.OrderID
 right join w3schools.products as prd on odd.productid = prd.ProductID
 where prd.CategoryID in (select cat.CategoryID from w3schools.categories as cat where cat.CategoryName = "beverages")
 group by emp.employeeid
-order by sold_most_beverages desc
-limit 10;
+order by sold_most_beverages desc;
