2a917902259ada5e39004b3e329c9ec9b93b14f9
Mahmoud Ezzat <98676093+mammhoud@users.noreply.github.com>
2024-02-01 13:00:27 +0200

    Updates sql

--- SQL & Data Modelling/fetches.sql
+++ SQL & Data Modelling/fetches.sql
@@ -1,25 +1,22 @@
 select 
 emp.FirstName as Employee_FirstName,
 emp.LastName as Employee_LastName,
--- concat(emp.firstname," ",emp.lastname) as name, 
 count(ord.employeeid) as Number_of_Orders
 from w3schools.employees as emp
-left join w3schools.orders ord on ord.employeeid = emp.employeeid
+inner join w3schools.orders ord on ord.employeeid = emp.employeeid
 group by emp.employeeid
 order by Number_of_orders desc
 limit 10;
 
-
 --------------------
 
 select emp.firstname as Employee_FirstName, emp.lastname as Employee_LastName, 
-case when sum(odd.quantity) is NULL then 0 
-else sum(odd.Quantity) end as Sold_Most_Beverages
+  sum(odd.Quantity) as Sold_Most_Beverages
 from w3schools.order_details as odd
 inner join w3schools.orders as ord on odd.OrderID = ord.OrderID
 inner join w3schools.products as prd on odd.productid = prd.ProductID
 inner JOIN w3schools.categories AS cat ON prd.CategoryID = cat.CategoryID and cat.CategoryName = "beverages"
-right join w3schools.employees as emp on emp.EmployeeID = ord.EmployeeID
+inner join w3schools.employees as emp on emp.EmployeeID = ord.EmployeeID
 
 group by emp.employeeid
 order by sold_most_beverages desc
